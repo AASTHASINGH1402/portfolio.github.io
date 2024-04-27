c4f87f8d20d3eddc9ae1c41cb7a53dcbf34c0f6f
AASTHASINGH1402 <aasvi7738@gmail.com>
2024-04-27 14:12:20 +0530

    done by aastha

---
+++ js/app.js
@@ -0,0 +1,7 @@
+//javascript here
+const hamburgerButton = document.querySelector('.hamburger');
+const hamburgerList = document.querySelector('.hamburger-list-container');
+
+hamburgerButton.addEventListener('click',()=>{
+    hamburgerList.classList.toggle('hidden')
+})
